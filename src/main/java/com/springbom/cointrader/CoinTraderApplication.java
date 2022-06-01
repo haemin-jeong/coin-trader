15c6abf6a20a1262fe41f7451e7b6de1a9d9cc85
jeonghaemin <jhm0828@gmail.com>
2022-06-01 20:10:52 +0900

    프로젝트 생성

---
+++ src/main/java/com/springbom/cointrader/CoinTraderApplication.java
@@ -0,0 +1,13 @@
+package com.springbom.cointrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinTraderApplication.class, args);
+    }
+
+}
