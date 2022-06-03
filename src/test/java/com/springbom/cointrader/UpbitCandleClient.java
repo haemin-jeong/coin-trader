006315f677eb41e4da5892c00ca3620fdffc680a
jeonghaemin <jhm0828@gmail.com>
2022-06-03 22:18:06 +0900

    Refactor: 요청 URI 생성 방식 변경
    기존 문자열을 조합하여 생성하던 방식에서 UriComponentBuilder를 사용하여 생성하는 방식으로 변경

--- src/test/java/com/springbom/cointrader/UpbitCandleClient.java
+++ src/test/java/com/springbom/cointrader/UpbitCandleClient.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -19,8 +22,20 @@ public class UpbitCandleClient {
     }
 
     public List<MinuteCandle> getMinuteCandles(MinuteType minuteType, MarketType marketType, int count, LocalDateTime to) {
-        String requestUrl = UrlBuilder.buildMinuteCandleUrl(minuteType.getMinute(), marketType, count, to);
-        ResponseEntity<List<MinuteCandle>> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<MinuteCandle>>() {});
+        URI uri = getUri(minuteType, marketType, count, to);
+        ResponseEntity<List<MinuteCandle>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<MinuteCandle>>() {});
         return response.getBody();
+    }
+
+    private URI getUri(MinuteType minuteType, MarketType marketType, int count, LocalDateTime to) {
+        return UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("api.upbit.com")
+                .path("/v1/candles/minutes/{minute}")
+                .queryParam("market", marketType.getUrlValue())
+                .queryParam("count", count)
+                .queryParam("to", to.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .buildAndExpand(minuteType.getMinute())
+                .toUri();
     }
 }
