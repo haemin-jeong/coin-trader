eb4a3a0934b5a07f0b67f9317a250aa522fe7dc3
jeonghaemin <jhm0828@gmail.com>
2022-06-14 20:39:44 +0900

    Feat: 백테스트 기능 구현

--- src/main/java/com/springbom/cointrader/app/AppRunner.java
+++ src/main/java/com/springbom/cointrader/app/AppRunner.java
@@ -3,7 +3,7 @@ package com.springbom.cointrader.app;
 import com.springbom.cointrader.backtester.BackTester;
 import com.springbom.cointrader.config.ExecuteMode;
 import com.springbom.cointrader.crawler.Crawler;
-import com.springbom.cointrader.trader.Trader;
+import com.springbom.cointrader.enums.MarketType;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +11,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Profile("!test")
 @Component
@@ -23,7 +25,6 @@ public class AppRunner implements ApplicationRunner {
     private final AppExiter exiter;
 
     private final Crawler crawler;
-    private final Trader trader;
     private final BackTester backTester;
 
     @Override
@@ -34,10 +35,10 @@ public class AppRunner implements ApplicationRunner {
                     crawler.run();
                     break;
                 case BACK_TEST:
-                    backTester.run();
-                    break;
-                case REAL_TRADE:
-                    trader.run();
+                    LocalDateTime startDate = LocalDateTime.of(2022, 1, 1, 9, 30);
+                    LocalDateTime endDate = LocalDateTime.of(2022, 1, 2, 23, 59);
+                    double initBalance = 1_000_000;
+                    backTester.run(MarketType.KRW_BTC, initBalance, 200, startDate, endDate);
                     break;
                 default:
                     throw new RuntimeException();
--- src/main/java/com/springbom/cointrader/backtester/BackTester.java
+++ src/main/java/com/springbom/cointrader/backtester/BackTester.java
@@ -1,11 +1,92 @@
 package com.springbom.cointrader.backtester;
 
+import com.springbom.cointrader.common.minutecandle.service.FiveMinuteCandleService;
+import com.springbom.cointrader.backtester.order.service.OrderService;
+import com.springbom.cointrader.backtester.strategy.Strategy;
+import com.springbom.cointrader.backtester.wallet.Wallet;
+import com.springbom.cointrader.backtester.wallet.WalletService;
+import com.springbom.cointrader.common.minutecandle.entity.FiveMinuteCandle;
+import com.springbom.cointrader.enums.MarketType;
+import com.springbom.cointrader.enums.MinuteType;
+import com.springbom.cointrader.util.rsi.RSIUtil;
+import com.springbom.cointrader.util.rsi.RSIs;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class BackTester {
 
-    public void run() {
-        System.out.println("BackTester.run");
+    private final FiveMinuteCandleService minuteCandleService;
+    private final WalletService walletService;
+    private final OrderService orderService;
+    private final Strategy strategy;
+
+    public void run(MarketType marketType, double initBalance, int limitCount, LocalDateTime startDate, LocalDateTime endDate) {
+        LocalDateTime targetDate = LocalDateTime.of(startDate.getYear(), startDate.getMonth(), startDate.getDayOfMonth(), 0, 0);
+        boolean isRun = true;
+
+        walletService.clear();
+        orderService.clear();
+
+        Wallet wallet = walletService.createWallet(marketType, initBalance);
+
+        while (isRun) {
+            FiveMinuteCandle baseCandle = minuteCandleService.findMinuteCandleByKstDate(marketType, targetDate);
+
+            if (Objects.isNull(baseCandle)) {
+                targetDate = targetDate.plusMinutes(MinuteType.ONE.getMinute());
+                continue;
+            }
+
+            //매수, 매도 기준이 되는 캔들
+            FiveMinuteCandle targetCandle = minuteCandleService.findNextMinuteCandle(marketType, baseCandle.getTimestamp());
+
+            //기준 시간으로부터 limitCount개의 캔들을 가져온다.
+            List<FiveMinuteCandle> minuteCandles = minuteCandleService.findMinutesCandleByTimestamp(marketType, baseCandle.getTimestamp(), limitCount);
+
+            List<Double> tradePrices = minuteCandles.stream().map(FiveMinuteCandle::getTradePrice).collect(Collectors.toUnmodifiableList());
+
+            RSIs rsi = RSIUtil.getRSI14(tradePrices);
+
+            runAskProcess(targetCandle, wallet, rsi);
+
+            runBidProcess(targetCandle, wallet, rsi);
+
+            wallet.fetch(targetCandle.getTradePrice());
+
+            if (checkRunCondition(endDate, targetCandle)) {
+                isRun = false;
+            }
+
+            targetDate = targetDate.plusMinutes(MinuteType.FIVE.getMinute());
+        }
+
+        walletService.updateWallet(wallet);
+    }
+
+    private boolean checkRunCondition(LocalDateTime endDate, FiveMinuteCandle targetCandle) {
+        return endDate.isBefore(targetCandle.getCandleDateTimeKst());
+    }
+
+    private void runBidProcess(FiveMinuteCandle targetCandle, Wallet wallet, RSIs rsi) {
+        if (wallet.isBidable() && strategy.isBidable(rsi)) {
+            orderService.bid(wallet, targetCandle.getOpeningPrice(), targetCandle.getCandleDateTimeKst());
+            walletService.bidWallet(wallet, targetCandle.getOpeningPrice());
+        }
+    }
+
+    private void runAskProcess(FiveMinuteCandle targetCandle, Wallet wallet, RSIs rsi) {
+        if (wallet.isAskable() && strategy.isAskable(rsi)) {
+            orderService.ask(wallet, targetCandle.getOpeningPrice(), targetCandle.getCandleDateTimeKst());
+            walletService.askWallet(wallet, targetCandle.getOpeningPrice());
+        }
     }
 }
