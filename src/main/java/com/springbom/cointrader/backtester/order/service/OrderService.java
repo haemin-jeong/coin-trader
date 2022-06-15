ab06803b01d7db1be75b8cec2ee9890d5f6d9626
jeonghaemin <jhm0828@gmail.com>
2022-06-15 19:29:13 +0900

    Refactor: OrderService ask,bid 메서드 반환 타입 변경

--- src/main/java/com/springbom/cointrader/backtester/order/service/OrderService.java
+++ src/main/java/com/springbom/cointrader/backtester/order/service/OrderService.java
@@ -1,14 +1,15 @@
 package com.springbom.cointrader.backtester.order.service;
 
+import com.springbom.cointrader.backtester.order.entity.Order;
 import com.springbom.cointrader.backtester.wallet.Wallet;
 
 import java.time.LocalDateTime;
 
 public interface OrderService {
 
-    void ask(Wallet wallet, Double tradePrice, LocalDateTime candleDatetime);
+    Order ask(Wallet wallet, Double tradePrice, LocalDateTime candleDatetime);
 
-    void bid(Wallet wallet, Double tradePrice, LocalDateTime candleDateTime);
+    Order bid(Wallet wallet, Double tradePrice, LocalDateTime candleDateTime);
 
     void clear();
 }
--- src/main/java/com/springbom/cointrader/backtester/order/service/OrderServiceImpl.java
+++ src/main/java/com/springbom/cointrader/backtester/order/service/OrderServiceImpl.java
@@ -21,7 +21,7 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
 
     @Transactional
-    public void ask(Wallet wallet, Double tradePrice, LocalDateTime candleDatetime) {
+    public Order ask(Wallet wallet, Double tradePrice, LocalDateTime candleDatetime) {
         double valuationAmount = wallet.getVolume() * tradePrice; // 평가금액 = 보유수량 * 현재가
         double commission = valuationAmount * commissionRate;
         double profits = (valuationAmount - commission) - wallet.getTotalPrice(); // 평가손익 = 평가금액 - 매수금액
@@ -32,11 +32,11 @@ public class OrderServiceImpl implements OrderService {
 
         log.info("[매도 주문] {}, 평가금액={}, 수익금={}, 수익률={}, 캔들시간={}", wallet.getMarket(), valuationAmount, profits, profitsRate, candleDatetime);
 
-        orderRepository.save(askingOrder);
+        return orderRepository.save(askingOrder);
     }
 
     @Transactional
-    public void bid(Wallet wallet, Double tradePrice, LocalDateTime candleDateTime) {
+    public Order bid(Wallet wallet, Double tradePrice, LocalDateTime candleDateTime) {
         double commission = wallet.getBalance() * commissionRate;
         double allBidPrice = wallet.getBalance() - commission;
         double bidVolume = allBidPrice / tradePrice;
@@ -46,7 +46,7 @@ public class OrderServiceImpl implements OrderService {
 
         log.info("[매수 주문] {}, 구매 개수={}, 총 구매 비용={}, 캔들시간={}", wallet.getMarket(), bidVolume, allBidPrice, candleDateTime);
 
-        orderRepository.save(biddingOrder);
+        return orderRepository.save(biddingOrder);
     }
 
     @Transactional
